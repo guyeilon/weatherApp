331524e15fd0ac4a44775f2c8c82ecb7fe5e8020
guyeilon <guyeilon84@gmail.com>
2022-08-09 09:10:38 +0300

    env files update

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
+import { env } from 'process';
 
 const weatherApi = axios.create({
 	baseURL: 'http://dataservice.accuweather.com',
 });
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const getLocationKey = async (geoString: string) => {
 	const res = await weatherApi.get('/locations/v1/cities/geoposition/search', {
 		params: {
 			// apikey: 'W6EwFbm24M1VCTf6e03m3u0U3kjsM66x',
-			apikey: 'Alh18uFM0BXPi8OTl1dg9E5KbeLiDKec',
+			apikey: API_KEY,
+			// apikey: 'Alh18uFM0BXPi8OTl1dg9E5KbeLiDKec',
 			q: geoString,
 		},
 	});
@@ -20,7 +24,8 @@ export const getDailyForecast = async (locationKey: string) => {
 	const res = await weatherApi.get(`forecasts/v1/daily/1day/${locationKey}`, {
 		params: {
 			// apikey: 'W6EwFbm24M1VCTf6e03m3u0U3kjsM66x',
-			apikey: 'Alh18uFM0BXPi8OTl1dg9E5KbeLiDKec',
+			// apikey: 'Alh18uFM0BXPi8OTl1dg9E5KbeLiDKec',
+			apikey: API_KEY,
 		},
 	});
 	const data = await res.data;
@@ -29,8 +34,9 @@ export const getDailyForecast = async (locationKey: string) => {
 export const getHourlyForecast = async (locationKey: string) => {
 	const res = await weatherApi.get(`forecasts/v1/hourly/24hour/${locationKey}`, {
 		params: {
-			apikey: 'W6EwFbm24M1VCTf6e03m3u0U3kjsM66x',
+			// apikey: 'W6EwFbm24M1VCTf6e03m3u0U3kjsM66x',
 			// apikey: 'Alh18uFM0BXPi8OTl1dg9E5KbeLiDKec',
+			apikey: API_KEY,
 		},
 	});
 	const data = await res.data;
@@ -40,7 +46,8 @@ export const getFiveDaysForecast = async (locationKey: string) => {
 	const res = await weatherApi.get(`forecasts/v1/daily/1day/${locationKey}`, {
 		params: {
 			// apikey: 'W6EwFbm24M1VCTf6e03m3u0U3kjsM66x',
-			apikey: 'Alh18uFM0BXPi8OTl1dg9E5KbeLiDKec',
+			// apikey: 'Alh18uFM0BXPi8OTl1dg9E5KbeLiDKec',
+			apikey: API_KEY,
 		},
 	});
 	const data = await res.data;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -10,11 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<Router>
+			<HashRouter>
 				<Routes>
 					<Route path='/*' element={<App />} />
 				</Routes>
-			</Router>
+			</HashRouter>
 			<ReactQueryDevtools initialIsOpen />
 		</QueryClientProvider>
 	</React.StrictMode>
