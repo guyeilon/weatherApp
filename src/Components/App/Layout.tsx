b10e00352632f55dad536013416035b7026b4c0d
guyeilon <guyeilon84@gmail.com>
2022-09-19 14:17:59 +0300

    .

--- src/Components/App/Layout.tsx
+++ src/Components/App/Layout.tsx
@@ -20,7 +20,7 @@ const Layout = () => {
 	return (
 		<>
 			<Wrapper>
-				{!mapOnMobile && <Clouds cloudsNum={10} />}
+				{/* {!mapOnMobile && <Clouds cloudsNum={10} />} */}
 				{!mapOnMobile && <Navbar />}
 				<Outlet />
 				<MobileFooter />
